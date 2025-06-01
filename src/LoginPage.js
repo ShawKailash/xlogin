30c5c063dc3c614140b10a357c9c7f002e44388f
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2025-06-01 08:37:47 +0000

    “xlogin”

---
+++ src/LoginPage.js
@@ -0,0 +1,52 @@
+import{ useState } from "react";
+
+function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(username === "" || password ===""){
+      setError("Both fields are required");
+      return;
+    }
+  
+    if(username === "user" && password === "password"){
+      setError("");
+      setIsSubmitted(true);
+    }
+    else{
+      setError("Invalid username or password");
+      setIsSubmitted(false);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h1>Login Page</h1>
+      {isSubmitted ? (
+        <div>
+          <p>Wellcome, {username}!</p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          {error && <p className="error">{error}</p>}
+          <div>
+            <lebel htmlFor="username">Username:</lebel>
+            <input type="text" id="username" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+          </div>
+          <div>
+            <lebel htmlFor="password">Password:</lebel>
+            <input type="password" id="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default LoginPage;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import XLogin from './App';
+import LoginPage from './LoginPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <XLogin />
+    <LoginPage />
   </React.StrictMode>
 );
 
